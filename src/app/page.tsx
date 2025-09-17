cb4301df6d30ef7e0061218cad16d93a7cbf26e6
sudeep prajapati <sudeeppprajapati@gmail.com>
2025-09-17 17:33:19 +0530

    Add Sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,26 @@
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuBadge, SidebarMenuButton, SidebarMenuItem, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebarClient } from "./_AppSideBarClient";
 
 export default function HomePage() {
   return (
-    <h1>Hello</h1>
-  );
+    <SidebarProvider className="overflow-y-hidden">
+      <AppSidebarClient>
+        <Sidebar collapsible="icon" className="overflow-hidden" >
+          <SidebarHeader className="flex-row">
+            <SidebarTrigger />
+            <span className="text-xl text-nowrap">Get Jobs</span>
+          </SidebarHeader>
+          <SidebarContent></SidebarContent>
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>Signup</SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+        <main className="flex-1">Jobs</main>
+      </AppSidebarClient>
+    </SidebarProvider>
+  )
 }
